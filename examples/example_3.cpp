f3dfcfc3ff785adb263892d8f2dea5eead220ba9
Amit Singh <amitsingh19975@gmail.com>
2025-03-28 21:19:35 +0530

    add a new example

---
+++ examples/example_3.cpp
@@ -0,0 +1,54 @@
+#include <chrono>
+#include <cstdlib>
+#include <print>
+
+#include "tpl.hpp"
+#include "tpl/algorithm.hpp"
+#include "tpl/thread.hpp"
+
+using namespace tpl;
+
+int main() {
+    Schedular s;
+    {
+        par::for_each<2>(s, range_t(0, 10, 1), [](range_t r) {
+            std::string tmp;
+            tmp += std::format("Running on: {}\n", ThisThread::get_native_id());
+            for (auto i = r.start; i < r.end; i+= r.stride) {
+                tmp += std::format("\tIter: {}\n", i); 
+            }
+            std::println("{}", tmp);
+        });
+        auto tmp = s.run();
+        if (!tmp) std::println("Error: {}", to_string(tmp.error()));
+    }
+
+    s.reset();
+
+    {
+        auto t = s.add_task([] () -> std::size_t {
+            for (auto i = 0ul; i < 4; ++i) {
+                std::println("Doing work... {}sec", i);
+                ThisThread::sleep_for(std::chrono::seconds(1));
+            }
+            return 10;
+        });
+        auto e0 = par::for_each<2>(s, range_t(0, 10, 1), t, [](range_t r, TaskToken& token) {
+            auto offset = std::get<0>(token.arg<std::size_t>()).value_or(0);
+            std::string tmp;
+
+            tmp += std::format("Running on: {}, Offset: {}\n", ThisThread::get_native_id(), offset);
+            for (auto i = r.start; i < r.end; i+= r.stride) {
+                tmp += std::format("\tIter: {}\n", i + offset); 
+            }
+            std::println("{}", tmp);
+        });
+
+        if (!e0) {
+            std::println("Error: {}", to_string(e0.error()));
+            return 1;
+        }
+        auto tmp = s.run();
+        if (!tmp) std::println("Error: {}", to_string(tmp.error()));
+    }
+}
