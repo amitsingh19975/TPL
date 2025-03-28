7739d12ad32491f21f9a69cb34f33d8b8d0ef30b
Amit Singh <amitsingh19975@gmail.com>
2025-03-28 16:19:44 +0530

    update test

--- test/runtime/value_store_test.cpp
+++ test/runtime/value_store_test.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <print>
+#include "tpl/task_id.hpp"
 #include "tpl/value_store.hpp"
 
 using namespace tpl;
@@ -12,40 +13,40 @@ TEST_CASE("Value Store", "[value_store]" ) {
         REQUIRE(store.size() == 0);
 
         WHEN("Ask for data at invalid index") {
-            auto maybe = store.get<int>(0);
+            auto maybe = store.get<int>(TaskId(0));
             REQUIRE(!maybe);
             REQUIRE(maybe.error() == ValueStoreError::not_found);
         }
 
         WHEN("Data is put inside the store") {
-            store.put(0, std::string("data"));
-            store.put(1, 10);
-            store.put(2, 12.23);
+            store.put(TaskId(0), std::string("data"));
+            store.put(TaskId(1), 10);
+            store.put(TaskId(2), 12.23);
             REQUIRE(store.size() == 3);
             REQUIRE(store.empty() == false);
 
             {
-                auto val = store.get<std::string>(0);
+                auto val = store.get<std::string>(TaskId(0));
                 REQUIRE(val.has_value());
                 REQUIRE(val.value().get() == "data");
                 REQUIRE(store.size() == 3);
             }
 
             {
-                auto val = store.consume<int>(1);
+                auto val = store.consume<int>(TaskId(1));
                 REQUIRE(val.has_value());
                 REQUIRE(val.value() == 10);
                 REQUIRE(store.size() == 2);
             }
 
             {
-                auto val = store.consume<int>(2);
+                auto val = store.consume<int>(TaskId(2));
                 REQUIRE(!val.has_value());
                 REQUIRE(store.size() == 2);
                 REQUIRE(val.error() == ValueStoreError::type_mismatch);
             }
 
-            store.remove(0);
+            store.remove(TaskId(0));
             REQUIRE(store.size() == 1);
             store.clear();
             REQUIRE(store.size() == 0);
