5629161d4b8fe97c54d569843e57968433c07511
Amit Singh <amitsingh19975@gmail.com>
2025-03-28 18:03:00 +0530

    fix example

--- examples/example_1.cpp
+++ examples/example_1.cpp
@@ -2,24 +2,8 @@
 #include <print>
 
 #include "tpl.hpp"
-#include "tpl/schedular.hpp"
 
 using namespace tpl;
-
-struct Test {
-    Test() = default;
-    Test(Test const&) {
-        std::println("Copying");
-    }
-    Test(Test &&) {
-        std::println("Moving");
-    }
-    Test& operator=(Test const&) = default;
-    Test& operator=(Test &&) = delete;
-    ~Test() {
-        std::println("Called ~Test()");
-    }
-};
 
 template <typename T, typename E>
 void print(std::expected<T, E> const& v) {
@@ -29,20 +13,21 @@ void print(std::expected<T, E> const& v) {
 
 int main() {
     Schedular s;
-    [[maybe_unused]] auto t0 = s.add_task([](TaskToken& token) {
+    auto t0 = s.add_task([](TaskToken& token) {
         auto value = token.arg<int>(TaskId(1)).value_or(-1);
         std::println("Hello from task 0: Called after => {}", value);
         token.stop();
+        return 0;
     });
     auto i = 0ul;
-    [[maybe_unused]] auto t1 = s.add_task([&i](TaskToken& token) {
+    auto t1 = s.add_task([&i](TaskToken& token) {
         std::println("[{}]: Hello from task 1", i);
         if (i ++ < 5) {
             token.schedule();
         }
         return 1;
     });
-    [[maybe_unused]] auto t2 = s.add_task([](TaskToken& token) {
+    auto t2 = s.add_task([](TaskToken& token) {
         auto value = token.arg<int>(TaskId(0)).value_or(-1);
         std::println("Hello from task 2: Called after => {}", value);
         return 2;
