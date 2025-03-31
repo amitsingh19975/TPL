8ca43fa81d53f7e60b4565b7142fa7c2b149c24a
Amit Singh <amitsingh19975@gmail.com>
2025-03-31 23:06:00 +0530

    update example

--- examples/example_5.cpp
+++ examples/example_5.cpp
@@ -11,10 +11,18 @@ int main() {
         | []{ std::println("Task 0: {}", ThisThread::pool_id()); }
         | []{ std::println("Task 1: {}", ThisThread::pool_id()); }
         | TaskGroup {
-           []{ std::println("Task 2: {}", ThisThread::pool_id()); },
-           []{ std::println("Task 3: {}", ThisThread::pool_id()); },
-           []{ std::println("Task 4: {}", ThisThread::pool_id()); }
-        } > []{ std::println("Task 1.5: {}", ThisThread::pool_id()); }
+           []{ return std::make_pair(0, ThisThread::pool_id()); },
+           []{ return std::make_pair(1, ThisThread::pool_id()); },
+           []{ return std::make_pair(2, ThisThread::pool_id()); },
+        } > [](TaskToken& t){
+            auto args = t.all_of<std::pair<int, std::size_t>>();
+            assert(args.size() == 3);
+
+            for (auto i = 0ul; i < args.size(); ++i) {
+                auto [a, b] = args[i].take();
+                std::println("Pair[{}]: {}", a, b);
+            }
+        }
         ;
 
     ts.run();
