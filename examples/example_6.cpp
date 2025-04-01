39322da6dba1dee4bdf7778b227671ae0cf59e98
Amit Singh <amitsingh19975@gmail.com>
2025-04-01 21:43:11 +0530

    add channel example

---
+++ examples/example_6.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <ctime>
+#include <print>
+
+#include "tpl.hpp"
+
+using namespace tpl;
+
+int main() {
+    Scheduler s;
+    bounded_channel_t<std::size_t, 64> ch;
+    auto ts = s
+        | [&ch] {
+            if (ch.is_closed()) return;
+            for (auto i = 0ul; i < 100; ++i) {
+                auto res = ch.send(i);
+                if (!res) {
+                    if (res.error() == ChannelError::closed) return;
+                }
+                ThisThread::sleep_for(std::chrono::milliseconds(rand() % 1000));
+            }
+            ch.close();
+        }
+        | [&ch] {
+            if (ch.is_closed()) return;
+            for (auto i = 0ul; i < 100; ++i) {
+                auto res = ch.send(i + 100);
+                if (!res) {
+                    if (res.error() == ChannelError::closed) return;
+                }
+                ThisThread::sleep_for(std::chrono::milliseconds(rand() % 100));
+            }
+        }
+        | [&ch](TaskToken& t) {
+            if (ch.is_closed() && ch.empty()) return;
+            auto val = ch.receive();
+            if (!val) {
+                return;
+            }
+            std::println("Item: {}", *val);
+            t.schedule();
+        };
+    ts.run();
+}
