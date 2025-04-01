063d45e14b190f90afe3566f7579e0b795f6702a
Amit Singh <amitsingh19975@gmail.com>
2025-04-01 19:35:48 +0530

    update example

--- examples/example_5.cpp
+++ examples/example_5.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
+#include <ctime>
 #include <print>
+#include <stdexcept>
 
 #include "tpl.hpp"
 
@@ -8,7 +10,15 @@ using namespace tpl;
 int main() {
     Scheduler s;
     auto ts = s
-        | []{ std::println("Task 0: {}", ThisThread::pool_id()); }
+        | []{
+            srand(static_cast<unsigned>(time(0)));
+            auto r = rand();
+            if (r % 2 == 0) throw std::runtime_error(std::format("Random number({}) is even!", r));
+            std::println("Task 0: {}", r);
+        }
+            + ErrorHandler([](std::exception const& e) {
+                std::println("Error From Task 0: {}", e.what());
+            })
         | []{ std::println("Task 1: {}", ThisThread::pool_id()); }
         | TaskGroup {
            []{ return std::make_pair(0, ThisThread::pool_id()); },
@@ -16,13 +26,15 @@ int main() {
            []{ return std::make_pair(2, ThisThread::pool_id()); },
         } > [](TaskToken& t){
             auto args = t.all_of<std::pair<int, std::size_t>>();
-            assert(args.size() == 3);
+            if (args.size() != 3) throw std::runtime_error("Args must be 3");
 
             for (auto i = 0ul; i < args.size(); ++i) {
                 auto [a, b] = args[i].take();
                 std::println("Pair[{}]: {}", a, b);
-            }
-        }
+            } 
+        } + ErrorHandler([](std::exception const& e) {
+            std::println("Error thrown when args is not 3: {}", e.what());
+        })
         ;
 
     ts.run();
